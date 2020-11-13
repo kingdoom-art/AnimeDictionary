d3e4580c73e59333a4bc6abb435b61cee745a20e
Бартов Евгений Игоревич <bartov-2016@mail.ru>
2020-11-13 12:16:25 +0500

    bartov1: закоментил парсер и сервер, клиента нет смысла, по скольку он не доделан

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+//собственно наш сервер
 const express = require("express");
 const app = express();
+//будущий коннект к базе
 const pg = require('pg');
+//стока к базе
 const conString = 'postgres://postgres:625325@localhost:5432/AnimeInfo';
 
+//клиент, с помощью которого будем отправлять запросы бд
 var client = new pg.Client(conString);
+//создаем подключение и включаем слушателя
 client.connect().then(()=>{
 	app.listen(25525, function(){
     console.log("Подключено");
@@ -21,17 +26,18 @@ app.get("/get_anime", function(request, response){
 		response.send(res.rows[0].get_anime_info);
 	})
 });
-
+//тестовый мето для проверки подключения клиента к серверу
 app.get("/", function(request, response){
 	console.log("/");
 	response.send(JSON.stringify({"answer":1}));
 	response.end();
 });
+//метод ошибок от парсера(если не удалось коннектнуться к базе)
 app.get("/error", function(request, response){
     let  message = request.query.error;
 	console.log(message);
 });
-// /load?page=
+//сам запуск парсера /load?page=
 app.get("/load", function(request, response){
     let page = request.query.page;
 	console.log("load page="+page);
